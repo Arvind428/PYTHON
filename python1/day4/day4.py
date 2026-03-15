0cbe9bdd9578594a3cd3e987f0a9b7c408338a60
Arvind428 <arvind592006@gmail.com>
2026-03-15 21:49:48 +0530

    new changes

--- python1/day4/day4.py
+++ python1/day4/day4.py
@@ -1,10 +1,35 @@
-def fun(fname, lname,cgpa,course):
-    print("Hi Im"+fname+lname+" B-Tech "+course+" with "+cgpa)
-fun('ARVIND','GOUD','40%','CSE')
+"""
+Built in Data types are
+Text Type:      str
+Numeric Types:  int,float,complex
+Sequence Types: list,tuple,range
+Mapping Types:  dict
+Set Types:      set,frozenset
+Boolean Type:   bool
+Binary Types: bytes,bytearray,memoryview
+None Type: NoneType
+"""
+#Getting The Data Type
+x=5
+print(type(x))
+#Setting the Data Type
+x="Hello World"
+#Setting the Specific Data Type
+x=str("Hello World")
+#Python casting
 
-def fun1(fn,ln,cgpa,course):
-    print("hi"+fn+ln+cgpa+course)
-fun1("arvind" "goud" "40" "CSE")
-#for the given list of strings find a str which is a palindromic string
-#take a list str and cnt the vowels in each str,  if the cnt of vowels is even print the str
-#find the unique(only once should repeat) values in aa given list by user without using sets
+#INTEGERS
+x=int(1)#x will be 1
+y=int(2.9)#y will be 2
+z=int("3")#z will be 3
+
+#FLOAT
+x=float(1)#x will be 1.0
+y=float(2.9)#y will be 2.8
+z=float("3")#z will be 3.0
+w=float("4.5")#w will be 4.5
+
+#STRINGS
+x=str("s1")#x will be 's1'
+y=str(2)#y will be '2'
+z=str(3.0)#z will be '3.0'
---
+++ python1/day5/problems.py
@@ -0,0 +1,12 @@
+#for the given list of strings find a str which is a palindromic string
+words=["arvind","mam","palindrome","vinniv","madam"]
+for word in words:
+    if word==word[::-1]:
+        print(word+" is a palindrome")
+#take a list str and cnt the vowels in each str,  if the cnt of vowels is even print the str
+words=["arvind","mam","palindrome","vinniv","madam"]
+vow="aeiouAEIOU"
+for char in word if char in vow:
+        print("x")
+
+        #find the unique(only once should repeat) values in aa given list by user without using sets
