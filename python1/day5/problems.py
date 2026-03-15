15ee47ead739807ce007d99181d7fbc264ee8e5e
Arvind428 <arvind592006@gmail.com>
2026-03-15 22:39:39 +0530

    if-else statements

--- python1/day5/problems.py
+++ python1/day5/problems.py
@@ -1,12 +1,31 @@
-#for the given list of strings find a str which is a palindromic string
-words=["arvind","mam","palindrome","vinniv","madam"]
-for word in words:
-    if word==word[::-1]:
-        print(word+" is a palindrome")
-#take a list str and cnt the vowels in each str,  if the cnt of vowels is even print the str
-words=["arvind","mam","palindrome","vinniv","madam"]
-vow="aeiouAEIOU"
-for char in word if char in vow:
-        print("x")
+#adult or not
+age =int(input())
+if age>18:
+    print("Adult")
+else:
+    print("not Adult")    
+# a,b are equal or not
+a= int(input())
+b= int(input())
 
-        #find the unique(only once should repeat) values in aa given list by user without using sets
+if a == b:
+     print("a and b are equal")
+else:
+    print("a and b are not equal")
+
+# num is - or +
+num = int(input())
+if num>0:
+    print("positive")
+elif num==0:
+    print("zero")
+else:
+    print("negative")
+###### drink
+    num = int(input())
+if age<20:
+    print("don't drink")   
+elif 20 < age >60:
+    print("you may drink")
+else:
+    print("alochol is not good for your health")       
