136944f3bd56b05824fcc4e273afd96f6c57d06b
Arvind428 <arvind592006@gmail.com>
2026-03-23 19:57:15 +0530

    today

--- python1/day14/problems.py
+++ python1/day14/problems.py
@@ -1,3 +1,18 @@
-N=int(input())
-for i in range(1,11):
-    print(i*N,end=" ")
+for letter in 'geeksforgeeks':
+    if letter=='e' or letter=='s':
+        continue# returns the statement to begining (i.e it doesnt print the next line)
+    print('current letter:',letter)
+
+#Break Statement
+for letter in 'geeksforgeeks':
+    if letter == 'e' or letter == 's':
+        break
+#this statement stoops the loop when the condition  is found
+print('Current Letter :', letter)
+
+#Pass Statement
+for letter in 'geeksforgeeks':
+    pass
+# in this the pass statement passes the loop until the very last
+# where we cant find dtring to pass so the loop stops passing and prints the last letter s 
+print('Last Letter :', letter)
