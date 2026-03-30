5209ebe7ed8ad0ad20374bd269417cb0fb6ddb58
Arvind428 <arvind592006@gmail.com>
2026-03-30 20:19:56 +0530

    add

---
+++ python1/day20/concept.py
@@ -0,0 +1,13 @@
+"""
+List is a built-in data structure used to store an ordered collection of items.
+They are dynamic, resizable and capable of storing multiple data types.
+Allows duplicate elements
+Mutable
+Creating a List
+Lists can be created in several ways, such as using square brackets [] , the list() constructor or by repeating elements.
+.Using Square Brackets: Square brackets [] are used to create a list directly.
+
+. Using list() Constructor: A list can also be created by passing an iterable 
+(such as tuple, string or another list) to the list() constructor.
+
+"""
