fbca7b7a49f16fa4eecc258546247325ac56d638
Arvind428 <arvind592006@gmail.com>
2026-04-03 12:39:30 +0530

    strings

--- day25/slicing.py
+++ day25/slicing.py
@@ -1,4 +1,19 @@
 #WHAT IS STRING SLICING
-str="Hello World"
+# string slicing lets you extract a portion of a string or work with only a specific part of it
 
-print(str[0])#h
+#Syntax 
+# string[start:stop]
+str="Hello World"
+print(str[0:4])#Hell
+#We can print from start by skipping start indice
+print(str[:4])#Hell
+#printing from start to end indice
+print(str[4:])#o world
+
+# we can also get every even sting index if wwe want
+#Synatx is string[start:stop:step]
+# example
+print(str[0:11:2])#HloWrd
+#useful tip
+#We can reverse a string by using this syntax
+print(str[::-1])
