81bdb1fd089b9088b798309671db4a69f8f6e0e5
Arvind428 <arvind592006@gmail.com>
2026-04-03 09:28:14 +0530

    Lists

--- day24/PLists.py
+++ day24/PLists.py
@@ -5,3 +5,19 @@ print(fruits)
 #Find Length of a list
 nums=[10,20,30,40,50]
 print(len(nums))
+
+#Access first and last elements on a list
+print("First : ",nums[0])
+print("Last : ",nums[-1])
+
+#Sum of all numbers
+nums=[1,2,3,4,5]
+total=0
+for i in nums:
+    total+=i
+print(total)
+
+test=[]
+test.append(5)
+test.append(20)
+print(test)
