7079f31a2e716dd739f93c46e9d0b40e509c892c
Arvind428 <arvind592006@gmail.com>
2026-04-07 19:24:07 +0530

    problems

---
+++ day27/g.py
@@ -0,0 +1,28 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        result = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        return result
+    
+
+class Solution(object):
+    def countOdds(self, low, high):
+        """
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        count = 0
+        for i in range(low, high + 1):
+            if i % 2 != 0:
+                count += 1
+        return count
+    
