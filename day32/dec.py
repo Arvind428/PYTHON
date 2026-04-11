eab8311cbf81d0cfb72ae86be964e39a41065d08
Arvind428 <arvind592006@gmail.com>
2026-04-11 22:41:52 +0530

    hi

--- day32/dec.py
+++ day32/dec.py
@@ -1,11 +1,3 @@
-def decorator(abc):
-    def bat3():
-        for i in range(10):   # give some value to n
-            if i % 2 == 0:
-                print("even number", i)
-        abc()
-    return bat3 
-@decorator
-def prime():
-    print("Hello")
-prime()
+a=int(input("enter a number"))
+if a%2==0:
+    print("even")
