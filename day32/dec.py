3f65cae23b4d2bb1daac3510c7567f2bbbc23bd4
Arvind428 <arvind592006@gmail.com>
2026-04-13 20:36:43 +0530

    1

--- day32/dec.py
+++ day32/dec.py
@@ -1,3 +1,19 @@
-a=int(input("enter a number"))
-if a%2==0:
-    print("even")
+def square_decorator(func):
+    def wrapper(a, b):
+        result = func(a, b)  
+        return result ** 2   
+    return wrapper
+
+
+@square_decorator
+def add(a, b):
+    return a + b
+
+
+@square_decorator
+def sub(a, b):
+    return a - b
+
+
+print("Square of add:", add(5, 3))  
+print("Square of sub:", sub(5, 3))   
---
+++ day34/cond.py
@@ -0,0 +1,33 @@
+"""
+Conditional Statements,or conditinals 
+
+We know that we only have two values in a boolean statements 
+they are True and False
+
+The comparision operators in Python:
+
+the operators are
+==,!=,>,<,>=,<=
+"""
+print(3 > 4) # False
+print(3 < 4) # True
+print(3 == 4) # False
+print(4 == 4) # True
+print(3 != 4) # True
+print(3 >= 4) # False
+print(3 <= 4) # True
+
+"""
+if condition:
+    pass # code to execute if condition is true
+    if condition is an expression that evaluates to True or False follwed by a colon:
+
+"""
+#Example of if statement
+age=56
+if age>18:
+    print("you are  adult")
+
+#intendation error comes when you try to write print staement right below the if statement
+if age<18:
+print(age)
