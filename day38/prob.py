4fb6e77b28cf0a52b20ba298bca2511d3024c530
Arvind428 <arvind592006@gmail.com>
2026-04-17 19:56:08 +0530

    func

--- day38/prob.py
+++ day38/prob.py
@@ -1,9 +1,3 @@
-# print 1 to n numbers using functions
-def num(n):
-    for i in range(1,n+1):
-        print(i)
-num(6)
-
 #using reccursion
 def mun(s):
     if s==0:
@@ -12,8 +6,9 @@ def mun(s):
     print(s)
 mun(5)
 
+
 def mu(s):
     if s==0:
-        return 0
+        return
     return s+mu(s-1)
 print(mu(5))
