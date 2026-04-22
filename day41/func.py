358fcf7a23d1eb60989c4be73adc297bfe9f8ad3
Arvind428 <arvind592006@gmail.com>
2026-04-22 17:10:31 +0530

    func

---
+++ day41/func.py
@@ -0,0 +1,18 @@
+#Create a #list [1, 2, 3] and add 4 to the end.
+lst = [1, 2, 3]
+lst.append(4)
+print(lst)
+#Insert 10 at index 1 in [5, 6, 7]lst = [5, 6, 7]
+lst.insert(1, 10)
+print(lst)
+#Remove
+lst = [1, 2, 3, 4]
+lst.remove(3)
+print(lst)
+#pop
+lst = [10, 20, 30]
+lst.pop()
+print(lst)
+#length
+lst = [1, 2, 3, 4, 5]
+print(len(lst))
