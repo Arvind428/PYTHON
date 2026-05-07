dfa63dae2f0a3d468b39ff165fe542bb48baa07b
Arvind428 <arvind592006@gmail.com>
2026-05-07 21:42:11 +0530

    new

--- day45/new.py
+++ day45/new.py
@@ -1,1 +1,20 @@
-print("i am arvind")
+# if file exists file overwrites 
+# if file doesn't -w creates it
+# file = open("hiaa.txt", "w")
+# file.write("Hi this is api")
+# file.close()
+
+
+# with open("hiaa.txt", "w") as f:
+#    f.write("this file is created using w")
+
+# file = open("hiaa.txt", "r")
+# print(file.read())
+# file.close()
+
+with open("hiaa.txt", "a") as f:
+    f.write(" new log added\n")
+
+# file = open("newfile1.txt", "x")
+# file.write("This file is created using x mode.")
+# file.close()
