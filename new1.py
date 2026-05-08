c5acda70ed9225c1a9af76a8b028a26b5de3682c
Arvind428 <arvind592006@gmail.com>
2026-05-08 21:21:26 +0530

    oops

---
+++ new1.py
@@ -0,0 +1,24 @@
+class course:
+
+    def __init__(self,name,rno):
+        self.name = name
+        self.rno = rno
+
+car1 = course("shriya","123")#project assign
+car2=course("arvind","24")
+print(car1.name, car1.rno)#external
+print(car2.name,car2.rno)
+
+class course:
+    pass
+
+car1 = course()
+car1.name = "shriya"
+car1.rno = "123"
+
+car2 = course()
+car2.name = "arvind"
+car2.rno = "24"
+
+print(car1.name, car1.rno)
+print(car2.name, car2.rno)
