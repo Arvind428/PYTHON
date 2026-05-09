cb3c04d4b4ea02d0456b902d2b00a02258906eae
Arvind428 <arvind592006@gmail.com>
2026-05-09 19:56:16 +0530

    oops

--- day45/new1.py
+++ day45/new1.py
@@ -9,16 +9,16 @@ car2=course("arvind","24")
 print(car1.name, car1.rno)#external
 print(car2.name,car2.rno)
 
-class course:
-    pass
+# class course:
+#     pass
 
-car1 = course()
-car1.name = "shriya"
-car1.rno = "123"
+# car1 = course()
+# car1.name = "shriya"
+# car1.rno = "123"
 
-car2 = course()
-car2.name = "arvind"
-car2.rno = "24"
+# car2 = course()
+# car2.name = "arvind"
+# car2.rno = "24"
 
-print(car1.name, car1.rno)
-print(car2.name, car2.rno)
+# print(car1.name, car1.rno)
+# print(car2.name, car2.rno)
