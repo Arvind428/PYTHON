b244d8e3bfb6b4a9f33389875b45473fc19aead2
Arvind428 <arvind592006@gmail.com>
2026-05-15 20:29:33 +0530

    class

--- day49/prob.py
+++ day49/prob.py
@@ -7,3 +7,47 @@ class dog(Animal):
         print("Dog is shouting")
 d1=dog()
 d1.sound()
+
+#p-payment, ch-upi, ch-card---method=pay(self, amnt)
+class payment:
+    def pay(self,amnt):
+        print("Select Payment method")
+class upi(payment):
+    def pay(self,amnt):
+        print("Selected upi:")
+        print("amount paid",amnt)
+class card(payment):
+    def pay(self,amnt):
+        print("Selected card:")
+        print("amount paid",amnt)
+u=upi()
+c=card()
+u.pay(100)
+c.pay(560)
+
+#p-employ, ch- developer, ch- manager classes---work method
+
+class employ:
+    def rule(self,id):
+        print("have to wear id")
+        print("your id is",id)
+class developer(employ):
+    def rule(self,id):
+        print("have to wear the developer id")
+        print("your id is",id)
+        if id=="developer":
+            print("you have the access for the code snippet")
+        print("does not have access ofdeveloper perks")
+class manager(employ):
+    def rule(self,id):
+        print("have to wear the manager id")
+        print("your id is",id)
+        if id=="manager":
+            print("have the connection of all the employees")
+        print("does not have the access of manager perks")
+e=employ()
+d=developer()
+m=manager()
+e.rule("employ")
+d.rule("developer")
+m.rule("manager")
