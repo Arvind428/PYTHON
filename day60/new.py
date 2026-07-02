4c9f20ca7c1ade9a20d89da945d3fff6c93b4647
Arvind428 <arvind592006@gmail.com>
2026-07-02 20:16:07 +0530

    lists

--- day60/new.py
+++ day60/new.py
@@ -15,3 +15,22 @@ else:
 #reverse a string
 text = input("Enter a string: ")
 print(text[::-1])
+
+#find the largest number in list
+numbers = [4, 7, 1, 9, 3]
+largest = numbers[0]
+for num in numbers:
+    if num > largest:
+        largest = num
+print(largest)
+
+#count vowels
+text = input("Enter a string: ")
+
+count = 0
+
+for ch in text.lower():
+    if ch in "aeiou":
+        count += 1
+print("Vowels:", count)
+
